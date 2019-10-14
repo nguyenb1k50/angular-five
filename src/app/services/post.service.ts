271324b76df5ddda8ee95750a34989ed3fbf6c03
MAI TRUNG NGUYEN <nguyenmt24@gmail.com>
2019-10-14 20:50:23 +0700

    copy from mosh

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,43 +1,10 @@
-import { BadInput } from 'src/app/common/bad-input';
-import { NotFoundError } from 'src/app/common/not-found-error';
-import { AppError } from './../common/app-error';
+import { DataService } from './data.service';
 import { Http } from '@angular/http';
 import { Injectable } from '@angular/core';
-import 'rxjs/add/operator/catch';
-import { Observable } from 'rxjs/observable'; 
 
-@Injectable({
-  providedIn: 'root'
-})
-export class PostService {
-  private url = 'http://jsonplaceholder.typicode.com/posts';
-
-  constructor(private http: Http) { }
-
-  getPost() {
-    return this.http.get(this.url);
-  }
-
-  createPost(post) {
-    return this.http.post(this.url, JSON.stringify(post))
-    .catch((error: Response) => {
-      if(error.status === 400) 
-        return Observable.throw(new BadInput(error.json()));
-
-      return Observable.throw(new AppError(error.json()));
-    });
-  }
-
-  updatePost(post) {
-    return this.http.patch(this.url + '/' + post.id, JSON.stringify({ isRead: true}));
-  }
-
-  deletePost(id){
-    return this.http.delete(this.url + '/' + id)
-    .catch((error: Response) => {
-      if(error.status === 404)
-        return Observable.throw(new NotFoundError());
-      return Observable.throw(new AppError(error));
-    });
-  }
+@Injectable()
+export class PostService extends DataService {
+  constructor(http: Http) {
+    super('http://jsonplaceholder.typicode.com/posts', http);
+   }
 }
